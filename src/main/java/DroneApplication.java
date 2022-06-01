e31bd737e3741f65fcafa326228c8828fb22416b
rajeev <rajeevth11@gmail.com>
2022-06-01 13:04:13 +0530

    Introduce schedule process

--- src/main/java/DroneApplication.java
+++ src/main/java/DroneApplication.java
@@ -1,3 +1,4 @@
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -5,19 +6,19 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import scheduler.BatteryMonitoringJob;
 
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan (basePackages = { "api.drone"})
-@EnableJpaRepositories(basePackages = "api.drone.repository")
+@EnableJpaRepositories(basePackages = {"api.drone.repository"})
 @EntityScan( basePackages = {"api.drone.model"})
 
 public class DroneApplication
 {
-
+    final static Logger logger = Logger.getLogger( DroneApplication.class );
     public static void main(String[] args)
     {
+        logger.log( Level.ERROR, "<<<<<<<<<<<<<<<<<<<<Starting Application>>>>>>>>>>>>>>>>>>>" );
         SpringApplication.run(DroneApplication.class, args);
     }
 }
--- src/main/java/api/drone/scheduler/BatteryMonitoringJob.java
+++ src/main/java/api/drone/scheduler/BatteryMonitoringJob.java
@@ -1,8 +1,7 @@
-package scheduler;
+package api.drone.scheduler;
 
 import api.drone.model.Drone;
 import api.drone.repository.DroneRepository;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,9 +12,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+
 @Component
-//@EnableScheduling
-//@EnableAsync
 public class BatteryMonitoringJob
 {
     final static Logger logger = Logger.getLogger( BatteryMonitoringJob.class );
@@ -23,18 +21,16 @@ public class BatteryMonitoringJob
     @Autowired
     private DroneRepository droneRepository;
 
-    @Scheduled(fixedRate = 100)
-    public void scheduleFixedRateTaskAsync() throws InterruptedException
+    //@Async
+    @Scheduled(fixedRate = 10000 )
+    public void batteryMonitor()
     {
+        SimpleDateFormat sdf = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss.SSS" );
+        String strDate = sdf.format( new Date() );
         List<Drone> droneList = droneRepository.findAll();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-        Date now = new Date();
-        String strDate = sdf.format(now);
-        DecimalFormat decFormatter = new DecimalFormat( "#%" );
         for( Drone drone : droneList )
         {
-            System.out.println("hiii");
-            logger.info( strDate + " >>>Drone SerialNumber : " + drone.getSerialNumber() + " Battery Capacity " + decFormatter.format( drone.getBatteryCapacity() ) );
+            logger.info( strDate + "Battery Level for Drone >>>> SerialNumber : " + drone.getSerialNumber() + " Battery Capacity " + drone.getBatteryCapacity() );
         }
     }
 }
